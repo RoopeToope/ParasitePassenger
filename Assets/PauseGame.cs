a646152aa320708ff433083aa701d86e0926ecec
srbackman <seba.backman@pp.inet.fi>
2022-11-06 18:30:36 +0200

    Update PauseGame.cs

--- Assets/PauseGame.cs
+++ Assets/PauseGame.cs
@@ -20,7 +20,7 @@ public class PauseGame : MonoBehaviour
             }
         }
     }
-    void Resume() {
+    public void Resume() {
         pauseMenuUI.SetActive(false);
 
         Time.timeScale = 1f;
